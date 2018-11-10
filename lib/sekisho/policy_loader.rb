849ab7c0659233cff00a32772e5f0b8febf36f76
okmth <contrabass.gliga@gmail.com>
2018-11-11 02:34:41 +0900

    Refactor

--- lib/sekisho/policy_loader.rb
+++ lib/sekisho/policy_loader.rb
@@ -10,16 +10,12 @@ module Sekisho
 
     def load!
       policy = Object.const_get(policy_class.class_name)
-      policy.instance_methods(!termination).include?(Sekisho.config.query_method) or
+      policy.instance_methods(termination).include?(Sekisho.config.query_method) or
         raise Errors::AuthorizedQueryMethodNotDefinedError.new(policy)
       policy
     rescue LoadError, NameError => e
       raise Errors::PolicyNotFoundError.new(policy_class) if termination
       self.class.new(policy_class, termination: true).load!
-    end
-
-    def query_method_defined?(policy)
-      policy.instance_methods(!termination).include?(Sekisho.config.query_method)
     end
   end
 end
